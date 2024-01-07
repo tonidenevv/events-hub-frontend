16ffd69780eae91bbbe0a2d90540e00cf9837957
Toni Denev <tonidenevbtc@gmail.com>
2024-01-07 16:10:08 +0200

    delete/change initial files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+      <h1 className='text-3xl text-center font-bold underline'>Hello World!</h1>
     </div>
   );
 }
