74d4041e214317e08046c4407505731ac1897437
Toni Denev <tonidenevbtc@gmail.com>
2024-01-20 13:28:05 +0200

    expired filter

--- src/components/Events/FilterEvents/FilterModal/ExpiredFilter/ExpiredFilter.js
+++ src/components/Events/FilterEvents/FilterModal/ExpiredFilter/ExpiredFilter.js
@@ -16,11 +16,13 @@ const ExpiredFilter = () => {
                 Expired/Non-Expired
             </div>
             <div className="flex justify-center items-center">
-                <div className="grid gap-3 grid-cols-2">
-                    <input checked={isChecked('tenAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="tenAndMore" value="tenAndMore" />
-                    <label htmlFor="tenAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('tenAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>10+</label>
-                    <input checked={isChecked('tenAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="tenAndMore" value="tenAndMore" />
-                    <label htmlFor="tenAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('tenAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>10+</label>
+                <div className="grid gap-3 grid-cols-3">
+                    <input checked={isChecked('anyExpiry')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="anyExpiry" value="anyExpiry" />
+                    <label htmlFor="anyExpiry" className={`cursor-pointer rounded-xl p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('anyExpiry') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Both</label>
+                    <input checked={isChecked('nonExpiredFilter')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="nonExpiredFilter" value="nonExpiredFilter" />
+                    <label htmlFor="nonExpiredFilter" className={`cursor-pointer rounded-xl p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('nonExpiredFilter') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Non-Expired</label>
+                    <input checked={isChecked('expiredFilter')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="expiredFilter" value="expiredFilter" />
+                    <label htmlFor="expiredFilter" className={`cursor-pointer rounded-xl p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('expiredFilter') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Expired</label>
                 </div>
             </div>
         </div>
