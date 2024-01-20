54dbd75c0778a2c1482f8f2790350b1a2dba3334
Toni Denev <tonidenevbtc@gmail.com>
2024-01-20 13:37:29 +0200

    change modal width

--- src/components/Events/FilterEvents/FilterModal/ExpiredFilter/ExpiredFilter.js
+++ src/components/Events/FilterEvents/FilterModal/ExpiredFilter/ExpiredFilter.js
@@ -16,13 +16,13 @@ const ExpiredFilter = () => {
                 Expired/Non-Expired
             </div>
             <div className="flex justify-center items-center">
-                <div className="grid gap-3 grid-cols-3">
+                <div className="grid lg:gap-3 gap-2 grid-cols-3">
                     <input checked={isChecked('anyExpiry')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="anyExpiry" value="anyExpiry" />
-                    <label htmlFor="anyExpiry" className={`cursor-pointer rounded-xl p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('anyExpiry') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Both</label>
+                    <label htmlFor="anyExpiry" className={`cursor-pointer rounded-xl text-md p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('anyExpiry') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Both</label>
                     <input checked={isChecked('nonExpiredFilter')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="nonExpiredFilter" value="nonExpiredFilter" />
-                    <label htmlFor="nonExpiredFilter" className={`cursor-pointer rounded-xl p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('nonExpiredFilter') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Non-Expired</label>
+                    <label htmlFor="nonExpiredFilter" className={`cursor-pointer rounded-xl text-md p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('nonExpiredFilter') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Non-Expired</label>
                     <input checked={isChecked('expiredFilter')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="expiredFilter" value="expiredFilter" />
-                    <label htmlFor="expiredFilter" className={`cursor-pointer rounded-xl p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('expiredFilter') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Expired</label>
+                    <label htmlFor="expiredFilter" className={`cursor-pointer rounded-xl text-md p-1 flex justify-center items-center text-center font-semibold border-2 ${isChecked('expiredFilter') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Expired</label>
                 </div>
             </div>
         </div>
--- src/components/Events/FilterEvents/FilterModal/FilterModal.js
+++ src/components/Events/FilterEvents/FilterModal/FilterModal.js
@@ -8,7 +8,7 @@ import PriceSlider from './PriceSlider/PriceSlider';
 const FilterModal = ({ closeFilterModal }) => {
     return (
         <div onClick={closeFilterModal} id="backdrop" className="bg-opacity-30 inset-0 z-50 backdrop-blur-sm fixed flex justify-center items-center">
-            <div className="bg-slate-100 border-2 rounded-lg flex flex-col lg:w-[28rem] w-80 h-[32rem] shadow-2xl border-black relative lg:rounded-sm overflow-y-auto">
+            <div className="bg-slate-100 border-2 rounded-lg flex flex-col lg:w-[28rem] w-[22rem] h-[32rem] shadow-2xl border-black relative lg:rounded-sm overflow-y-auto">
                 <div>
                     <div className='absolute right-2 top-2 z-10 hover:text-gray-500 cursor-pointer ease-in-out duration-100 rounded-full'><CloseLogo /></div>
                     <div className="text-center top-2 absolute right-0 left-0 border-b-2 pb-0.5 text-lg border-slate-300">Filters</div>
