f764cd1a85be7d1677cfcedb7d9da236cb137f30
Toni Denev <tonidenevbtc@gmail.com>
2024-01-20 13:15:31 +0200

    center grid elements in their container

--- src/components/Events/FilterEvents/FilterModal/AttendingCountFilter/AttendingCountFilter.js
+++ src/components/Events/FilterEvents/FilterModal/AttendingCountFilter/AttendingCountFilter.js
@@ -15,17 +15,19 @@ const AttendingCountFilter = () => {
             <div className="font-semibold my-4 ml-8 text-lg">
                 Attending Count
             </div>
-            <div className="grid grid-cols-5 justify-center items-center px-3 gap-3">
-                <input checked={isChecked('anyAttending')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="anyAttending" value="anyAttending" />
-                <label htmlFor="anyAttending" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center text-sm font-semibold border-2 ${isChecked('anyAttending') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Any</label>
-                <input checked={isChecked('oneAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="oneAndMore" value="oneAndMore" />
-                <label htmlFor="oneAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('oneAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>1+</label>
-                <input checked={isChecked('threeAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="threeAndMore" value="threeAndMore" />
-                <label htmlFor="threeAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('threeAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>3+</label>
-                <input checked={isChecked('fiveAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="fiveAndMore" value="fiveAndMore" />
-                <label htmlFor="fiveAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('fiveAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>5+</label>
-                <input checked={isChecked('tenAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="tenAndMore" value="tenAndMore" />
-                <label htmlFor="tenAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('tenAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>10+</label>
+            <div className="flex justify-center items-center">
+                <div className="grid grid-cols-5 px-3 gap-6 lg:gap-10">
+                    <input checked={isChecked('anyAttending')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="anyAttending" value="anyAttending" />
+                    <label htmlFor="anyAttending" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center text-sm font-semibold border-2 ${isChecked('anyAttending') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Any</label>
+                    <input checked={isChecked('oneAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="oneAndMore" value="oneAndMore" />
+                    <label htmlFor="oneAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('oneAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>1+</label>
+                    <input checked={isChecked('threeAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="threeAndMore" value="threeAndMore" />
+                    <label htmlFor="threeAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('threeAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>3+</label>
+                    <input checked={isChecked('fiveAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="fiveAndMore" value="fiveAndMore" />
+                    <label htmlFor="fiveAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('fiveAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>5+</label>
+                    <input checked={isChecked('tenAndMore')} onChange={handleRadioChange} className="hidden" type="radio" name="attendingCount" id="tenAndMore" value="tenAndMore" />
+                    <label htmlFor="tenAndMore" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('tenAndMore') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>10+</label>
+                </div>
             </div>
         </div>
     )
--- src/components/Events/FilterEvents/FilterModal/DaysLeftFilter/DaysLeftFilter.js
+++ src/components/Events/FilterEvents/FilterModal/DaysLeftFilter/DaysLeftFilter.js
@@ -16,17 +16,19 @@ const DaysLeftFilter = () => {
             <div className="font-semibold my-4 ml-8 text-lg">
                 Days until the Event
             </div>
-            <div className="grid grid-cols-5 px-3 gap-3">
-                <input checked={isChecked('anyRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="anyRadio" value="anyRadio" />
-                <label htmlFor="anyRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center text-sm font-semibold border-2 ${isChecked('anyRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Any</label>
-                <input checked={isChecked('oneDayRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="oneDayRadio" value="oneDayRadio" />
-                <label htmlFor="oneDayRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('oneDayRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>1</label>
-                <input checked={isChecked('twoDayRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="twoDayRadio" value="twoDayRadio" />
-                <label htmlFor="twoDayRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('twoDayRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>2</label>
-                <input checked={isChecked('threeDayRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="threeDayRadio" value="threeDayRadio" />
-                <label htmlFor="threeDayRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('threeDayRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>3</label>
-                <input checked={isChecked('fourPlusRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="fourPlusRadio" value="fourPlusRadio" />
-                <label htmlFor="fourPlusRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('fourPlusRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>4+</label>
+            <div className="flex justify-center items-center">
+                <div className="grid grid-cols-5 px-3 gap-6 lg:gap-10">
+                    <input checked={isChecked('anyRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="anyRadio" value="anyRadio" />
+                    <label htmlFor="anyRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center text-sm font-semibold border-2 ${isChecked('anyRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>Any</label>
+                    <input checked={isChecked('oneDayRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="oneDayRadio" value="oneDayRadio" />
+                    <label htmlFor="oneDayRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('oneDayRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>1</label>
+                    <input checked={isChecked('twoDayRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="twoDayRadio" value="twoDayRadio" />
+                    <label htmlFor="twoDayRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('twoDayRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>2</label>
+                    <input checked={isChecked('threeDayRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="threeDayRadio" value="threeDayRadio" />
+                    <label htmlFor="threeDayRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('threeDayRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>3</label>
+                    <input checked={isChecked('fourPlusRadio')} onChange={handleRadioChange} className="hidden" type="radio" name="daysLeft" id="fourPlusRadio" value="fourPlusRadio" />
+                    <label htmlFor="fourPlusRadio" className={`cursor-pointer rounded-full h-9 w-9 flex justify-center items-center text-center font-semibold border-2 ${isChecked('fourPlusRadio') ? 'bg-black border-white text-white' : 'bg-white border-black text-black hover:bg-slate-200'}`}>4+</label>
+                </div>
             </div>
         </div>
     )
