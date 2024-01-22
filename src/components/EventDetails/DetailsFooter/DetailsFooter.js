b02ca083a5fa0bbffcf4bd8c71ed6707b5e129aa
Toni Denev <tonidenevbtc@gmail.com>
2024-01-22 16:48:40 +0200

    footer on event details page

---
+++ src/components/EventDetails/DetailsFooter/DetailsFooter.js
@@ -0,0 +1,19 @@
+import { format } from "date-fns";
+
+const DetailsFooter = ({ date, ticketPrice }) => {
+    return (
+        <div className="sticky bottom-0 border-t-2 p-3 shadow-2xl items-center border-gray-200 bg-white grid grid-cols-2">
+            <div className="font-semibold flex justify-center items-center flex-col text-lg lg:text-xl">
+                <div className="flex flex-col items-start">
+                    <div>${ticketPrice}</div>
+                    <div>{format(date, 'd MMM y')}</div>
+                </div>
+            </div>
+            <div className="flex justify-center items-center">
+                <button className="bg-pink-700 text-white lg:text-xl font-semibold px-3 py-1.5 border-2 border-black rounded-lg hover:bg-pink-900 ease-in-out duration-150">Attend</button>
+            </div>
+        </div>
+    )
+}
+
+export default DetailsFooter;
--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -4,14 +4,20 @@ import * as eventService from '../../services/eventService';
 import { useContext } from "react";
 import { ToastContext } from "../../contexts/ToastContext";
 import Spinner from "../Spinner/Spinner";
+import ChevronLeft from "../svg/ChevronLeft";
+import DetailsFooter from "./DetailsFooter/DetailsFooter";
 
 const EventDetails = () => {
     const [event, setEvent] = useState({});
     const [isLoading, setIsLoading] = useState(false);
+    const [date, setDate] = useState(new Date());
     const { eventId } = useParams();
 
     const { showToast } = useContext(ToastContext);
     const navigate = useNavigate();
+
+    // console.log(format(event.eventDate, "yyyy-MM-dd"))
+    // console.log(event.eventDate);
 
     useEffect(() => {
         setIsLoading(true);
@@ -20,6 +26,7 @@ const EventDetails = () => {
                 if (res.message) return navigate('/404')
                 setIsLoading(false);
                 setEvent(res);
+                setDate(res.eventDate);
             })
             .catch(err => {
                 setIsLoading(false);
@@ -28,16 +35,34 @@ const EventDetails = () => {
             })
     }, [eventId, navigate, showToast]);
 
+    const handleBackClick = () => {
+        navigate('/events');
+    }
+
+    console.log(event);
+
     return (
         isLoading ? <Spinner /> :
-            <div className="flex justify-center">
-                <div className="lg:w-8/12 flex items-center flex-col">
-                    <div className="mt-10 lg:ml-0 ml-2 font-bold text-4xl">{event.title}</div>
-                    <div className="mt-4 rounded-2xl lg:w-3/6 w-11/12 h-96 cursor-pointer">
-                        <img className="w-full h-full rounded-2xl shadow-2xl hover:brightness-75" src={event.imageUrl} alt="Event" />
+            <>
+                <div className="flex justify-center relative">
+                    <button onClick={handleBackClick} className="lg:left-6 lg:top-6 left-3 top-3 absolute flex cursor-pointer hover:text-gray-600"><ChevronLeft />Back</button>
+                    <div className="lg:w-8/12 flex mt-8 lg:mt-0 items-center flex-col">
+                        <div className="flex w-full h-full flex-col items-center">
+                            <div className="lg:mt-10 mt-6 text-center order-2 lg:ml-0 ml-2 font-bold text-4xl lg:text-5xl">{event.title}</div>
+                            <div className="mt-4 rounded-2xl lg:order-2 lg:w-3/6 w-11/12 h-96 cursor-pointer">
+                                <img className="w-full h-full rounded-2xl shadow-2xl hover:brightness-75" src={event.imageUrl} alt="Event" />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="flex items-start mt-10">
+                    <div className="w-5/6 lg:ml-16 ml-6">
+                        <div className="font-bold text-2xl lg:text-4xl">About The Event</div>
+                        <div className="font-semibold text-2xl text-gray-700 mt-4 lg:text-3xl">{event.description}</div>
+                    </div>
+                </div>
+                <DetailsFooter date={date} ticketPrice={event.ticketPrice} />
+            </>
     )
 }
 
