09922d2c0911b968079ccc18288b67a44171c9df
Toni Denev <tonidenevbtc@gmail.com>
2024-01-25 11:55:32 +0200

    owners can comment under own post
    guests see special message when trying to comment

--- src/components/EventDetails/CommentSection/CommentSection.js
+++ src/components/EventDetails/CommentSection/CommentSection.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Tick from "../../svg/Tick";
 import NoComments from "./NoComments/NoComments";
 import * as commentService from '../../../services/commentService';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { ToastContext } from "../../../contexts/ToastContext";
 import { useContext } from "react";
@@ -45,12 +45,12 @@ const CommentSection = ({ comments, event, handleComment, isOwner }) => {
                 <div>
                     {error && <div className="text-sm lg:text-lg text-red-500">Comment has to be between 3 and 20 characters.</div>}
                 </div>
-                {!isOwner && user
+                {user
                     ? <div className="relative">
                         <input type="text" className={`py-1.5 px-2 lg:w-96 w-64 border-2 text-base focus:outline-none focus:border-blue-500 ${error ? 'border-red-500' : 'border-black'} rounded-lg m-3 ml-0`} value={comment} onChange={(e) => setComment(e.target.value)} placeholder="Write a comment..!" />
                         <button onClick={handleSubmit} className="absolute right-5 hover:text-green-500 rounded-full flex items-center justify-center top-0 bottom-0 text-sm"><Tick /></button>
                     </div>
-                    : null
+                    : <div className="text-base"><Link className="text-blue-500 mr-1" to="/login">Log in</Link>to be able to comment and like comments!</div>
                 }
                 {comments?.length === 0
                     ? <NoComments />
